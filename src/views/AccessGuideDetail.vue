ce6541fd616ce90c833e6e4a5abfd48ee615f5bd
xuyixiao <768069291@qq.com>
2019-12-10 17:25:32 +0800

    first commit

--- src/views/AccessGuideDetail.vue
+++ src/views/AccessGuideDetail.vue
@@ -10,7 +10,7 @@
             </Breadcrumb>
         </div>
         <div class="select-lab">
-             <Menu :theme="theme3" active-name="1" width="340px" class="menu">
+             <Menu :theme="theme3" active-name="1" width="240px" class="menu">
                 <MenuItem name="1" class="menuitem">
                     <p>接入前必读</p>
                 </MenuItem>
@@ -26,7 +26,7 @@
             </Menu>
         </div>
     </div>
-    <Footer class="footer"></Footer>
+    <!-- <Footer class="footer"></Footer> -->
 </div>
 </template>
 
@@ -48,51 +48,44 @@ export default {
 </script>
 
 <style scoped>
-.body {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-}
+@import url("../assets/styles/font/font.css");
 .footer {
-    /* position: fixed;
-    bottom: 0px; */
-    flex: 0 0 auto;
+    position: relative;
 }
 .accessguidedetail {
-    width:1920px;
-    height:2700px;
+    width:100%;
+    /* height:2700px; */
+    position: relative;
+    height: auto;
     background:rgba(255,255,255,1);
 }
 .breadcrumbitem{
-    font-size:20px;
+    font-size:1em;
     font-family:PingFangSC-Regular,PingFang SC;
     line-height:28px;
 }
 .productintro-title {
     position: absolute;
-    top: 166px;
+    top: 32px;
     left: 160px;
 }
 .select-lab {
     position: absolute;
-    top: 234px;
-    left: 161px;
+    top: 75px;
+    left: 151px;
 }
 .menuitem {
-    width: 340px;
-    height: 99px;
-    /* background:#e8f6f7; */
+    width: 240px;
+    height: 75px;
 }
 .menuitem p{
-    position: absolute;
-    top: 32px;
-    left: 120px;
-    /* width:104px;
-    height:37px; */
-    font-size:26px;
+    text-align: center;
+    position: relative;
+    top: 5px;
+    height:37px;
+    font-size:1.32em;
     font-family:PingFangSC-Medium,PingFang SC;
     font-weight:500;
-    color:rgba(74,74,74,1);
     line-height:37px;
 }
 </style>
