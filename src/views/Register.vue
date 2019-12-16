07aba68638690ea0a4c4789265675a0feba31da7
xuyixiao <768069291@qq.com>
2019-12-16 14:24:36 +0800

    zhuce

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,14 @@
 <template>
-<div class="register-box">
-    <img src="/images/ztkj.png">
+<div class="body">
+    <div class="head-title">
+        <img src="/images/ztkj.png">
     <div class="box-right">
         <span>已有账号，</span>
         <a href="/login">马上登录</a><a href="/">返回首页</a>
         <a class="line" href="#"></a>
     </div>
+    </div>
+    <div class="register-box">    
     <RegisterOk class="register-body2" v-show="jump"></RegisterOk>
     <div class="register-body" v-show="!jump">
         <div class="body-title">
@@ -14,13 +17,13 @@
         <div class="register-input">
             <input type="text"  placeholder="请输入注册的手机号码" v-model="phone"  
             @input="$emit('input',$event.target.value)" :error="phone"> 
-            <!-- 滑动解锁模块 -->
+            <!-- 滑动解锁模块 <Icon type="ios-lock-outline" />-->
             <div class="lock">
                 <drag-verify 
-            :width="460"
-            :height="60"
-            handlerIcon='fa fa-angle-double-right'
-            successIcon='fa fa-check'
+            :width="327"
+            :height="45"
+            handlerIcon='fa fa-lock'
+            successIcon='fa fa-unlock-alt'
             text="请将滑块拖动到右侧解锁"
             success-text="已解锁" 
             background="#ddd"
@@ -46,12 +49,12 @@
             </p>
         </div>
         <button class="create-btn" :disabled="isClick" @click="goLogin">创建用户</button>
-        <div v-if="error" class="invalid-feedback">{{error}}</div>
     </div>
-    <div class="foot-font"><p>Copyright© 2018赞同科技|京ICP备 14027589号-1</p></div>
+    
+</div>
+<div class="foot-font"><p>Copyright© 2018赞同科技|京ICP备 14027589号-1</p></div>
 </div>
 </template>
-
 <script>
 import dragVerify from 'vue-drag-verify'//滑动解锁插件
 import RegisterOk from './RegisterOk'
@@ -85,7 +88,8 @@ export default {
         },
         //获取验证码
         getVerifyCode() {
-            if (this.validatePhone()) {
+            if (this.$refs.Verify.isPassing) {
+                if (this.validatePhone()) {
                 //发送请求
                 this.validateBtn();
                 this.$axios.post("/api/posts/sms_send",{
@@ -96,6 +100,10 @@ export default {
                 .then(res => {
                     console.log(res);
                 });
+            }
+            } else {
+                this.error = '请先滑动解锁'
+                this.showError();
             }
         },
         //“获取验证码按钮”
@@ -118,10 +126,12 @@ export default {
             if (!this.phone) {
                 this.error = 
                     "手机号码不能为空!" ;
+                    this.showError();
                 return false;
             } else if (!/^1[345678]\d{9}$/.test(this.phone)) {
                 this.error = 
                     "请填写正确的手机号码!";
+                    this.showError();
                 return false;
             } else {
                 this.error = '';
@@ -133,13 +143,16 @@ export default {
             if (!this.verifyCode) {
                 this.error = 
                     "请输入验证码!" ;
+                    this.showError();
                 return false;
             } else if (!/^[a-zA-Z0-9]{6}$/.test(this.verifyCode)) {
                 this.error = 
                     "验证码错误!";
+                    this.showError();
                 return false;
             } else if(!document.getElementById('agreement-check').checked) {
                 this.error = "请勾选用户注册协议！"
+                this.showError();
             }else {
                 this.error = '';
                 return true;
@@ -149,6 +162,10 @@ export default {
         toShowCode () {
             this.pwdType = this.pwdType === 'password'?'text':'password';
             this.seen = !this.seen;//小眼睛的变化
+        },
+        //错误提示弹出
+        showError(){
+            this.$Message.error(this.error);
         }
     },
     computed:{
@@ -173,210 +190,192 @@ export default {
 </script>
 
 <style scoped>
-.register-box {
-    width:1920px;
-    /* width: auto; */
-    height:1080px;
+@import url("../assets/styles/font/font.css");
+.body{
+    width: 100%;
+    min-width: 1200px;
     background:rgba(240,240,240,1);
+    text-align: center;
+    height: 900px;
 }
-.register-box img:nth-of-type(1) {
+.head-title {
+    width: 711.5px;
+    height: 85px;
+    display: table;
+    margin: 0 auto;
+    position: relative;
+    top: 60px;
+}
+.head-title img{
+    width: 185px;
+    height: 100%;
     position: absolute;
-    width:260px;
-    height:120px;
-    top: 82px;
-    right: 1225px;
-    left: 435px;
-    bottom: 878px;
+    left: 0px;
 }
 .box-right {
+    width: 214px;
     position: absolute;
     height:28px;
-    font-size:20px;
+    font-size:1em;
     font-family:PingFangSC-Regular,PingFang SC;
     font-weight:400;
-    top: 134px;
-    bottom: 918px;
+    right: 0px;
 }
 .box-right span {
     position: absolute;
-    width:100px;
+    width:71px;
     color:rgba(29,31,49,1);
     line-height:28px;
-    left: 1160px;
-    right: 660px;
+    left: 0px;
+    bottom: -30px;
 }
 .box-right a:nth-of-type(1) {
     position: absolute;
-    width:100px;
     color:rgba(2,164,175,1);
     line-height:28px;
-    left: 1260px;
-    right: 580px;;
+    left: 71px;
+    bottom: -30px;
 }
 .box-right a:nth-of-type(2) {
     position: absolute;
-    width:80px;
     color:rgba(2,164,175,1);
     line-height:28px;
-    left: 1380px;
-    right: 460px;
+    right: 0px;
+    bottom: -30px;
 }
-/* .box-right a:nth-of-type(3) { */
 .line {
     position: absolute;
     width:1px;
-    height:14px;
+    height:10px;
     background:rgba(2,164,175,1);
     border-radius:1px;
-    top: 7px;
-    /* bottom: 918px; */
-    left: 1359px;
-    right: 560px;
+    right: 70px;
+    bottom: -23px;
 }
-.register-body2 {
-    position:absolute;
-    width:1000px;
-    height:674px;
-    background:rgba(255,255,255,1);
-    box-shadow:0px 2px 40px 0px rgba(248,248,248,1);
-    border-radius:10px;
-    top: 203px;
-    right: 460px;
-    left: 460px;
-    bottom: 203px;
+.register-box {
+    width:711.5px;
+    background:rgba(240,240,240,1);
+    display: table;
+    margin: 0 auto;
 }
 .register-body {
-    position:absolute;
-    width:1000px;
-    height:674px;
+    position: relative;
+    width:711px;
+    height:520px;
     background:rgba(255,255,255,1);
     box-shadow:0px 2px 40px 0px rgba(248,248,248,1);
     border-radius:10px;
-    top: 203px;
-    right: 460px;
-    left: 460px;
-    bottom: 203px;
+    display: table;
+    margin: 0 auto;
+    top: 60px;
+
 }
 .body-title {
     position: absolute;
-    width:1000px;
-    height:100px;
+    width:100%;
+    height:71px;
     background:rgba(29,31,49,1);
     border-radius:10px 10px 0px 0px;
     border:1px solid rgba(151,151,151,1);
-    bottom: 574px;
+    top: 0px;
 }
 .body-title span {
-    position: absolute;
+    position: relative;
     width:104px;
     height:37px;
-    font-size:26px;
+    font-size:1.4em;
     font-family:PingFangSC-Medium,PingFang SC;
     font-weight:500;
     color:rgba(255,255,255,1);
     line-height:37px;
-    top: 31px;
-    right: 448px;
-    left: 448px;
-    bottom: 32px;
+    display: table;
+    margin: 0 auto;
+    top: 15px;
+}
+.register-input input{
+    position: relative;
+    background:rgba(248,248,248,1);
+    border-radius:4px;
+    border:1px solid rgba(213,213,213,1);
 }
 .register-input input:nth-of-type(1){
-    position: absolute;
-    width:460px;
-    height:60px;
-    background:rgba(248,248,248,1);
-    border-radius:4px;
-    border:1px solid rgba(213,213,213,1);
-    top: 170px;
-    right: 270px;
-    left: 270px;
-    bottom: 444px;
+    position: relative;
+    width:327px;
+    height:45px;
+    
+    margin-left: auto;
+    margin-right: auto;
+    top: 120px;
 }
 .lock {
-    position: absolute;
-    top: 250px;
-    right: 270px;
-    left: 270px;
-    bottom: 340px;
-    
+    position: relative;
+    top: 140px;
+    display: table;
+    margin: 0 auto;
 }
 .register-input input:nth-of-type(2){
-    position: absolute;
-    width:280px;
-    height:60px;
-    background:rgba(248,248,248,1);
-    border-radius:4px;
-    border:1px solid rgba(213,213,213,1);
-    top: 330px;
-    right: 450px;
-    left: 270px;
-    bottom: 284px;
+    position: relative;
+    width:200px;
+    height:45px;
+    top: 160px;
+    left: -62px;
 }
 .register-input input:nth-of-type(3){
-    position: absolute;
-    width:460px;
-    height:60px;
-    background:rgba(248,248,248,1);
-    border-radius:4px;
-    border:1px solid rgba(213,213,213,1);
-    top: 410px;
-    right: 270px;
-    left: 270px;
-    bottom: 204px;
+    position: relative;
+    width:327px;
+    height:45px;
+    display: table;
+    margin: 0 auto;
+    top: 170px;
 }
 .register-input button {
     border: none;
     position: absolute;
-    width:160px;
-    height:60px;
+    width:110px;
+    height:45px;
     background:rgba(115,215,225,1);
     border-radius:4px;
-    top: 330px;
-    right: 270px;
-    left: 570px;
-    bottom: 284px;
+    top: 250px;
+    right: 190px;
 }
 .register-body img:nth-of-type(1) {
-    width:40px;
-    height:25px;
+    width:25px;
+    height:15px;
     position: absolute;
-    left: 670px;
-    right: 290px;
-    top: 430px;
-    bottom: 219px;
+    left: 480px;
+    bottom: 180px;
+}
+.agreement {
+    position: relative;
+    width: 327px;
+    margin-right: auto;
+    margin-left: auto;
+    top: 190px;
 }
 .agreement input{
     position: absolute;
-    top: 505px;
-    bottom: 155px;
-    left: 275px;
-    right: 711px;
+    left: 5px;
 }
 .agreement p {
-    width:354px;
+    width:300px;
     height:22px;
-    font-size:16px;
+    font-size:0.8em;
     font-family:PingFangSC-Regular,PingFang SC;
     font-weight:400;
     color:rgba(29,31,49,1);
     line-height:22px;
     position: absolute;
-    top: 500px;
-    bottom: 160px;
-    left: 304px;
-    right: 392px;
+    top: -5px;
+    left: 14px;
 }
 .create-btn {
     border: none;
-    width:460px;
-    height:60px;
+    width:327px;
+    height:45px;
     background:rgba(115,215,225,1);
     border-radius:4px;
-    position: absolute;
-    top: 554px;
-    bottom: 60px;
-    left: 270px;
-    right: 270px;
+    position: relative;
+    top: 234px;
 }
 .invalid-feedback {
     padding-top: 5px;
@@ -386,7 +385,7 @@ export default {
     height:22px;
     font-size:16px;
     top:180px;
-    left: 730px;
+    left: 430px;
 }
 .foot-font {
     width:372px;
@@ -396,10 +395,13 @@ export default {
     font-weight:400;
     color:rgba(155,155,155,1);
     line-height:22px;
-    position: absolute;
-    top: 917px;
-    bottom: 141px;
-    left: 770px;
-    right: 778px;
+    position: relative;
+    top: 117px;
+    display: table;
+    margin: 0 auto;
+}
+input::-webkit-input-placeholder{
+    position: relative;
+    left: 20px;
 }
 </style>
--- src/views/RegisterOk.vue
+++ src/views/RegisterOk.vue
@@ -22,69 +22,64 @@ export default {
 </script>
 
 <style scoped>
-/* .register-body {
-    position:absolute;
-    width:1000px;
-    height:674px;
+.register-body {
+    position:relative;
+    width:711px;
+    height:320px;
     background:rgba(255,255,255,1);
     box-shadow:0px 2px 40px 0px rgba(248,248,248,1);
     border-radius:10px;
-    top: 203px;
-    right: 460px;
-    left: 460px;
-    bottom: 203px;
-} */
+    top: 60px;
+    display: table;
+    margin: 0 auto;
+}
 .body-title {
     position: absolute;
-    width:1000px;
-    height:100px;
+    width:100%;
+    height:71px;
     background:rgba(29,31,49,1);
     border-radius:10px 10px 0px 0px;
     border:1px solid rgba(151,151,151,1);
-    bottom: 574px;
+    top: 0px;
 }
 .body-title span {
-    position: absolute;
+    position: relative;
     width:104px;
     height:37px;
-    font-size:26px;
+    font-size:1.4em;
     font-family:PingFangSC-Medium,PingFang SC;
     font-weight:500;
     color:rgba(255,255,255,1);
     line-height:37px;
-    top: 31px;
-    right: 448px;
-    left: 448px;
-    bottom: 32px;
+    display: table;
+    margin: 0 auto;
+    top: 15px;
 }
 .circle {
-    position: absolute;
-    /* width:130px;
-    height:130px; */
-    /* border:6px solid rgba(184,233,134,1); */
-    left: 435px;
-    top: 141px;
+    position: relative;
+    text-align: center;
+    top: 100px;
 }
 .success-text {
-    position: absolute;
+    position: relative;
     width:192px;
     height:45px;
-    font-size:32px;
+    font-size:2em;
     font-family:PingFangSC-Medium,PingFang SC;
     font-weight:500;
     color:rgba(29,31,49,1);
     line-height:45px;
-    top: 306px;
-    left: 404px;
+    display: table;
+    margin: 0 auto;
+    top: 150px;
 }
 .gologin-btn {
     border: none;
-    width:460px;
-    height:60px;
+    width:327px;
+    height:45px;
     background:rgba(2,164,175,1);
     border-radius:4px;
-    position: absolute;
-    top: 394px;
-    left: 270px;
+    position: relative;
+    top: 200px;
 }
 </style>
